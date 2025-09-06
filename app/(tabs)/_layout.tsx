3996d4453ea5d8ef78883aa0727fd2fd6979cfc2
FacuAlonso <112171600+FacuAlonso@users.noreply.github.com>
2025-09-05 22:27:09 -0300

    updates

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,12 +2,16 @@ import { Tabs } from "expo-router";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabsLayout() {
-  return (<Tabs screenOptions={{headerShown: false}}>
+  return (<Tabs screenOptions={{headerShown: false, animation: "shift"}}>
+
         <Tabs.Screen name="contador" options={{ title: 'Contador',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="sort-numeric-asc" color={color} />}}/>
-        <Tabs.Screen name="perfil" options={{ title: 'Perfil',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />}}/>
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="sort-numeric-asc" color={color}/>}}/>
+
         <Tabs.Screen name="tarjetas" options={{ title: 'Tarjetas',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="hand-pointer-o" color={color} />}}/>
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="hand-pointer-o" color={color}/>}}/>
+
+        <Tabs.Screen name="perfil" options={{ title: 'Perfil',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color}/>}}/>
+
   </Tabs>);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,39 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View style={styles.viewStyle}>
-      <Text style={{fontSize:25, fontWeight:'bold'}}>¡Bienvenido!</Text>
-      <Link style={styles.launchPressableStyle} href={'/(tabs)/contador'}>
+      <Text style={{ fontSize: 25, fontWeight: "bold" }}>¡Bienvenido!</Text>
+
+      <Pressable
+        style={styles.launchPressableStyle}
+        onPress={() => router.replace("/(tabs)/contador")}
+      >
         <Text style={styles.launchButtonTextStyle}>INGRESAR</Text>
-    </Link>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  viewStyle:{
+  viewStyle: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  launchButtonTextStyle:{
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 20
+  launchButtonTextStyle: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
   },
-  launchPressableStyle:{
-    backgroundColor: '#0052b1ff',
-    paddingHorizontal: '8%',
-    paddingVertical: '4%',
+  launchPressableStyle: {
+    backgroundColor: "#0052b1ff",
+    paddingHorizontal: "8%",
+    paddingVertical: "4%",
     borderRadius: 8,
-    marginVertical: '15%'
-  }
-})
-
-
+    marginVertical: "15%",
+  },
+});
